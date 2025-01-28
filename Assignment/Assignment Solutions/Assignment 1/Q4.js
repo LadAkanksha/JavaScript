fdb68c1882d05433b03ce66dbcbc0eae0db455d6
LadAkanksha <akankshalad2003@gmail.com>
2025-01-28 22:54:41 +0530

    Assignment 01

---
+++ Assignment/Assignment Solutions/Assignment 1/Q4.js
@@ -0,0 +1,21 @@
+// Accept one number and check whether is is divisible by 5 or not.
+
+function Check(num){
+    if(num % 5 == 0)
+    {
+        return true;
+    }
+    else
+    return false;
+}
+
+let no = prompt("Enter Any Number : ");
+let ans = Check(no);
+if(ans == true)
+{
+    console.log("Number is divisible by 5");
+}
+else
+{
+    console.log("Number is not divisible by 5");
+}
