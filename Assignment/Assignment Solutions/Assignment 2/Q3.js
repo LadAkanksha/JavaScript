3b2f53a19093985ca5e38ca6ed31ac7c83137949
LadAkanksha <akankshalad2003@gmail.com>
2025-01-29 23:30:49 +0530

    Assignment 02

---
+++ Assignment/Assignment Solutions/Assignment 2/Q3.js
@@ -0,0 +1,15 @@
+// 3. Accept on number from user if number is less than 10 then print
+//“Hello” otherwise print “Demo”. 
+
+function Display(no)
+{
+    if(no < 10)
+        console.log("Hello");
+    else
+        console.log("Demo");
+}
+
+// let Num = prompt("Enter a Number :");
+// Display(Num);
+
+Display(45);
