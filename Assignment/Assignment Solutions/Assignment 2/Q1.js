d8ec7677f84f8dba4b888a9a3b91e88ab0af2a4b
LadAkanksha <akankshalad2003@gmail.com>
2025-01-29 23:30:20 +0530

    Assignment 02

---
+++ Assignment/Assignment Solutions/Assignment 2/Q1.js
@@ -0,0 +1,14 @@
+// 1.Accept one number from user and print that number of * on screen. 
+
+function Display(no){
+    let Cnt = 0;
+    while( Cnt < no)
+    {
+        console.log(" * ");
+        ++Cnt;
+    }
+}
+// let num = prompt("\nEnter Any Number : ");
+// Display(num);        
+
+Display(10);
