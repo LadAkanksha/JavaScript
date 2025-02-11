d03d95c52c44d65e6adc156fae9060c4d9ead839
LadAkanksha <akankshalad2003@gmail.com>
2025-02-11 22:22:03 +0530

    Assignment 11

---
+++ Assignment/Assignment Solutions/Assignment 11/Q2.js
@@ -0,0 +1,41 @@
+/*
+2. Write a program which accept range from user and display all even numbers in
+between that range.
+
+Input : 23 35
+Output : 24 26 28 30 32 34
+
+Input : 10 18
+Output : 10 12 14 16 18
+
+Input : 10 10
+Output : 10
+
+Input : -10 2
+Output : -10 -8 -6 -4 -2 0 2
+
+Input : 90 18
+Output : Invalid range
+*/
+
+function RangeDisplayEven(start,end)
+{
+    if(start > end)
+    {
+        console.log("Invalid Range !!!");
+        return;
+    }
+
+    for(let i = start; i <= end ; i++)
+    {
+        if(i%2 === 0)
+        {
+            console.log(i);
+        }
+    }
+}
+
+let s = parseInt(prompt("Enter Starting Point : "));
+let e = parseInt(prompt("Enter Ending Point : "));
+
+RangeDisplayEven(s,e);
