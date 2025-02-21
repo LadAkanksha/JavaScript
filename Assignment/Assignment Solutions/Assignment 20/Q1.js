fda3d0d532c5b85b125d4037e992d7aa7d52a7c2
LadAkanksha <akankshalad2003@gmail.com>
2025-02-21 22:07:37 +0530

    Assignment 20

---
+++ Assignment/Assignment Solutions/Assignment 20/Q1.js
@@ -0,0 +1,36 @@
+/*
+1. Accept number of rows and number of columns from user and display below
+pattern.
+
+Input : iRow = 4 iCol = 4
+Output :
+            * * * #
+            * * # *
+            * # * *
+            # * * *
+*/
+
+function Pattern(row , column)
+{
+    for(let i = 1 ; i <= row ; i++)
+    {
+        for(let j = 1 ; j <= column ; j++)
+        {
+            if(i + j == column+1)
+            {
+                document.write(" # ");
+            }
+            else
+            {
+                document.write(" *5
+                     ");
+            }
+        }
+        document.write("<br>");
+    }
+}
+
+let r = parseInt(prompt("Enter Number Of Rows : "));
+let c = parseInt(prompt("Enter Number Of Columns : "));
+
+Pattern(r,c);
